9bb16dcd41bfe8588f3a0ed0583d19888f9e44f2
Dylan Romansky <dromansk@NutHouse.localdomain>
2018-10-26 20:48:27 -0700

    solved strcpy and strncpy issues

--- srcs/ft_strcpy.c
+++ srcs/ft_strcpy.c
@@ -14,8 +14,11 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	while (*src)
-		*(dst++) = *(src++);
-	*dst = '\0';
+	int i;
+
+	i = -1;
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = '\0';
 	return (dst);
 }
--- srcs/ft_strncpy.c
+++ srcs/ft_strncpy.c
@@ -14,8 +14,11 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
+	int i;
+
+	i = -1;
 	while (len--)
-		*(dst++) = *(src++);
-	*dst = '\0';
+		dst[i] = src[i];
+	dst[i] = '\0';
 	return (dst);
 }
