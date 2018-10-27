bbea684dd9009a8888e989bf21e912261b393829
Dylan Romansky <dromansk@NutHouse.localdomain>
2018-10-26 20:56:05 -0700

    odd error fixed

--- srcs/ft_strncpy.c
+++ srcs/ft_strncpy.c
@@ -14,10 +14,10 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	int i;
+	size_t i;
 
 	i = -1;
-	while (len--)
+	while (++i < (len))
 		dst[i] = src[i];
 	dst[i] = '\0';
 	return (dst);
