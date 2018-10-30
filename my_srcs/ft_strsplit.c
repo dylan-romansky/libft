7ca6b2f6e9f8a082ac9f75a3f8c451d45dadcd5e
Dylan Romansky <dromansk@e1z3r2p19.42.us.org>
2018-10-30 15:36:04 -0700

    changed strsplit method

--- my_srcs/ft_strsplit.c
+++ my_srcs/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 23:06:12 by dromansk          #+#    #+#             */
-/*   Updated: 2018/10/30 13:37:30 by dromansk         ###   ########.fr       */
+/*   Updated: 2018/10/30 15:34:48 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static size_t	ft_wordcount(char const *rts, char f)
 {
 	size_t wee;
 
-	wee = 1;
+	wee = 0;
 	while (*rts)
 	{
 		rts += ft_skipdelim(rts, f);
@@ -52,27 +52,24 @@ static size_t	ft_wordcount(char const *rts, char f)
 char			**ft_strsplit(char const *s, char c)
 {
 	int		a;
-	int		bee;
 	char	**n;
 	int		lim;
 
 	a = 0;
-	bee = 0;
 	lim = (int)ft_wordcount(s, c);
-	if ((n = (char **)malloc(sizeof(char *) * lim)))
+	if ((n = (char **)malloc(sizeof(char *) * (lim + 1))))
 	{
-		while (s[a] && a < (int)ft_strlen(s) && bee < lim)
+		while (lim--)
 		{
-			a += ft_skipdelim((s + a), c);
-			if (s[a] && a < (int)ft_strlen(s) && bee < lim)
-			{
-				n[bee] = (char *)malloc(sizeof(char)
-						* ft_wordlen((s + a), c) + 1);
-				ft_memcpy(n[bee++], (s + a), (ft_wordlen((s + a), c) + 1));
-				a += (ft_wordlen((s + a), c) + 1);
-			}
+			while (*s == c && *s)
+				s++;
+			n[a] = ft_strsub(s, 0, ft_wordlen(s, c));
+			if (n[a] == NULL)
+					return (NULL);
+			s += ft_wordlen(s, c);
+			a++;
 		}
-		n[lim] = 0;
+		n[a] = 0;
 		return (n);
 	}
 	return (NULL);
