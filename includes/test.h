eb0a806ca4549b0f3dbdfc3edebdf3c87119df5d
Dylan Romansky <dromansk@e1z3r2p19.42.us.org>
2018-10-30 17:33:30 -0700

    list test started

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/24 18:51:59 by dromansk          #+#    #+#             */
-/*   Updated: 2018/10/29 20:44:41 by dromansk         ###   ########.fr       */
+/*   Updated: 2018/10/30 17:27:14 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void	ft_testoddities(void);
 void	ft_testmem2(void);
 void	ft_teststr2(void);
 void	ft_testoddities2(void);
+void	ft_testlst(void);
 
 #endif
