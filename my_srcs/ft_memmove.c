1d07edbc52a628a410e3c5bfbeca255654498939
Dylan Romansky <dromansk@e1z3r8p2.42.us.org>
2018-11-02 13:47:43 -0700

    interesting

--- my_srcs/ft_memmove.c
+++ my_srcs/ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 16:20:15 by dromansk          #+#    #+#             */
-/*   Updated: 2018/11/01 16:21:20 by dromansk         ###   ########.fr       */
+/*   Updated: 2018/11/02 13:47:32 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memmove(void *dst, const void *src, size_t len)
+/*void		*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char *t;
 
@@ -24,4 +24,22 @@ void		*ft_memmove(void *dst, const void *src, size_t len)
 	if (ft_strncmp(dst, src, len) == 0)
 		return (dst);
 	return (NULL);
+}*/
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*srcc;
+	char	*dstc;
+	size_t	i;
+
+	i = -1;
+	srcc = (char *)src;
+	dstc = (char *)dst;
+	if (srcc < dstc)
+		while ((int)(--len) >= 0)
+			*(dstc + len) = *(srcc + len);
+	else
+		while (++i < len)
+			*(dstc + i) = *(srcc + i);
+	return (dst);
 }
