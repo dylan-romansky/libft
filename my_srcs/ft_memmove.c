1ccb9cd608f361f6665e48ada6639f93a9ff6a5c
Dylan Romansky <dromansk@e1z3r5p20.42.us.org>
2018-11-08 16:14:42 -0800

    final update to my functions

--- my_srcs/ft_memmove.c
+++ my_srcs/ft_memmove.c
@@ -6,40 +6,26 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 16:20:15 by dromansk          #+#    #+#             */
-/*   Updated: 2018/11/02 13:47:32 by dromansk         ###   ########.fr       */
+/*   Updated: 2018/11/08 16:09:18 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*void		*ft_memmove(void *dst, const void *src, size_t len)
-{
-	char *t;
-
-	if (!(t = (char *)malloc(sizeof(char) * len)))
-		return (NULL);
-	ft_memcpy(t, src, len);
-	ft_memcpy(dst, t, len);
-	free(t);
-	if (ft_strncmp(dst, src, len) == 0)
-		return (dst);
-	return (NULL);
-}*/
-
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*srcc;
-	char	*dstc;
+	char	*s;
+	char	*d;
 	size_t	i;
 
 	i = -1;
-	srcc = (char *)src;
-	dstc = (char *)dst;
-	if (srcc < dstc)
+	s = (char *)src;
+	d = (char *)dst;
+	if (s < d)
 		while ((int)(--len) >= 0)
-			*(dstc + len) = *(srcc + len);
+			*(d + len) = *(s + len);
 	else
 		while (++i < len)
-			*(dstc + i) = *(srcc + i);
+			*(d + i) = *(s + i);
 	return (dst);
 }
